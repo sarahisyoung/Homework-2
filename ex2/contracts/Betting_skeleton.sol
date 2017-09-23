7ec0900ba045b80f586bcf89e83080066de516c3
Sarah Young <sarahyoung@Sarahs-MacBook-Pro.local>
2017-09-23 16:34:08 -0700

    how do i do this

--- ex2/contracts/Betting_skeleton.sol
+++ ex2/contracts/Betting_skeleton.sol
@@ -7,6 +7,7 @@ contract BettingContract {
 	address public gamblerB;
 	address public oracle;
 	uint[] outcomes;
+	uint winner;
 
 	/* Structs are custom data structures with self-defined parameters */
 	struct Bet {
@@ -25,39 +26,86 @@ contract BettingContract {
 	event BetClosed();
 
 	/* Uh Oh, what are these? */
-	modifier OwnerOnly() {}
-	modifier OracleOnly() {}
+	modifier OwnerOnly() {
+	    if (msg.sender == owner) {
+            _;
+        }
+	    
+	}
+	modifier OracleOnly() {
+	    if (msg.sender == oracle) {
+            _;
+        }
+	}
 
 	/* Constructor function, where owner and outcomes are set */
 	function BettingContract(uint[] _outcomes) {
+	    owner = msg.sender;
+		outcomes = _outcomes;
+		
 	}
 
 	/* Owner chooses their trusted Oracle */
 	function chooseOracle(address _oracle) OwnerOnly() returns (address) {
+	    oracle = _oracle;
+	    return oracle;
 	}
 
 	/* Gamblers place their bets, preferably after calling checkOutcomes */
 	function makeBet(uint _outcome) payable returns (bool) {
+	    address g;
+	    if (gamblerA == 0) {
+	        g = gamblerA;
+	    } else {
+	        g = gamblerB;
+	    }
+	    
+	    bets[g] = Bet(_outcome, msg.value, true);
+	    BetMade(g);
+	    return true;
+	
+
+		
 	}
 
 	/* The oracle chooses which outcome wins */
 	function makeDecision(uint _outcome) OracleOnly() {
+	    uint total = bets[gamblerA].amount + bets[gamblerB].amount;
+	    if (bets[gamblerA].outcome == _outcome && bets[gamblerB].outcome != _outcome) {
+	        winnings[gamblerA] += total;
+	    }else if (bets[gamblerA].outcome != _outcome && bets[gamblerB].outcome == _outcome) {
+	        winnings[gamblerB] += total;
+	    } else if (bets[gamblerA].outcome != _outcome && bets[gamblerB].outcome != _outcome) {
+	        winnings[oracle] += total;
+	    } 
+	    BetClosed();
 	}
 
 	/* Allow anyone to withdraw their winnings safely (if they have enough) */
 	function withdraw(uint withdrawAmount) returns (uint remainingBal) {
+	    if (winnings[msg.sender] > withdrawAmount) {
+	        winnings[msg.sender] -= withdrawAmount;
+	        return remainingBal;
+	    }
 	}
 	
 	/* Allow anyone to check the outcomes they can bet on */
 	function checkOutcomes() constant returns (uint[]) {
+	    return outcomes;
 	}
 	
 	/* Allow anyone to check if they won any bets */
 	function checkWinnings() constant returns(uint) {
+	    return winnings[msg.sender];
 	}
 
 	/* Call delete() to reset certain state variables. Which ones? That's upto you to decide */
 	function contractReset() private {
+	    delete(gamblerA);
+	    delete(gamblerB);
+	    
+	    
+	    
 	}
 
 	/* Fallback function */
